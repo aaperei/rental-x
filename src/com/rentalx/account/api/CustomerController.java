bf13215efc1e40c3dd811ad0ab4b4484471248fd
PC01404S <PC01404S@corp.ad.emb>
2016-07-08 11:44:37 -0300

    Start Front-end

--- src/com/rentalx/account/api/CustomerController.java
+++ src/com/rentalx/account/api/CustomerController.java
@@ -1,10 +1,16 @@
 package com.rentalx.account.api;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping (value = "api/customer")
+@RequestMapping(value = "api/customer")
 public class CustomerController {
+
+	@RequestMapping(value = "/test", method = RequestMethod.POST)
+	public void testController() {
+		System.out.println("Controller funfou");
+	}
 
 }
